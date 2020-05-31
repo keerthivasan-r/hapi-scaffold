ac900e638199e673820752ac320ede9755084e6e
Keerthivasan Ramanathan <keerthivasan.dev@gmail.com>
2020-05-31 17:36:07 +0530

    Add sample handler for user

---
+++ handlers/user-handler.js
@@ -0,0 +1,26 @@
+function createUser(request,h){
+
+}
+
+
+function readUser(request,h){
+
+}
+
+
+function updateUser(request,h){
+
+}
+
+
+function deactivateUser(request,h){
+
+}
+
+
+module.exports = {
+    createUser,
+    readUser,
+    updateUser,
+    deactivateUser
+}
