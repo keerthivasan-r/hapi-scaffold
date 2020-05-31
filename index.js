f41b6cd12b3886e37df6b30b5f836d34d187ae03
Keerthivasan Ramanathan <keerthivasan.dev@gmail.com>
2020-05-31 17:12:52 +0530

    initial setup - hapijs server

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = new Hapi.server({
+        host : '0.0.0.0',
+        port : '3000'
+    });
+    
+    await server.start();
+    console.log('Server running on %s',server.info.uri);
+
+};
+
+
+process.on('unhandledRejection',(err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
